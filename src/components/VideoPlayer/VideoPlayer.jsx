a9c69018a7190f3af56b784c4a988a29245eef47
Hayford <sbqsekta@gmail.com>
2024-12-14 07:42:00 -0800

    Done

---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import '.VideoPlayer.css'
+// // import video from '../../assets/college-video.mp4'
+// const VideoPlayer = ({playState, setPlayState}) => {
+//   return (
+//     <div className={'VideoPlayer ${playState ? '':'hide'}' }>
+//         <video src={video} autoPlay muted controls></video>
+//     </div>
+//   )
+// }
+
+// export default VideoPlayer
